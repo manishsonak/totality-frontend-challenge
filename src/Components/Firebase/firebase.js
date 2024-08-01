24b97ecbc81b2741b58ebfa6175685619d61f58b
manishsonak <manishsonak9@gmail.com>
2024-08-01 13:10:43 +0530

    config remove

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7G9R94ahd-C3MfixamxuUHXq8ZcizC_U",
-  authDomain: "property-rentel.firebaseapp.com",
-  projectId: "property-rentel",
-  storageBucket: "property-rentel.appspot.com",
-  messagingSenderId: "703395698864",
-  appId: "1:703395698864:web:1dc58146cfa14d38904375",
-  measurementId: "G-J3BERW2X1E"
+  apiKey:'Your api key' ,
+  authDomain: "your auth Domain key" ,
+  projectId:  "Your Project id",
+  storageBucket:  'Your storageBucket',
+  messagingSenderId: "your messagingSender id",
+  appId:  "your appId",
+  measurementId: "Your measurementId",
 };
 
 
